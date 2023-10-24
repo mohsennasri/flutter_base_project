4eecf2fdd77e84857e3e0e75c2ba597f4ff0d204
Mohsen Nasri <mohsen.nasri@live.com>
2023-10-24 23:10:08 +0330

    .

--- lib/dependency_injection/domain/domain_layer_injection.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter_base_project/dependency_injection/domain/modules/usecase_module.dart';
-
-mixin DomainLayerInjection {
-  static Future<void> configureDomainLayerInjection() async {
-    await UseCaseModule.configureUseCaseModuleInjection();
-  }
-}
--- lib/dependency_injection/presentation/presentation_layer_injection.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter_base_project/dependency_injection/presentation/modules/bloc_module.dart';
-
-mixin PresentationLayerInjection {
-  static Future<void> configurePresentationLayerInjection() async {
-    await BlocModule.configureStoreModuleInjection();
-  }
-}
